d7af1d3511b084d75fb2d0ae6405fae9650094a3
𝘚.𝘊. 𝘓𝘰𝘳𝘦𝘯𝘵𝘻 <113468900+SCLorentz@users.noreply.github.com>
2025-06-28 11:45:32 -0300

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "meu flake.nix para configurações UwU";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+    nixosConfigurations.old = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
